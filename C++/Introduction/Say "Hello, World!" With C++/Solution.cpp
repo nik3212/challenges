77ada1eb61e9a20c1ab8e6f7ed93e24434e2e941
Nikita Vasilev <nv3212@gmail.com>
2018-11-08 00:22:39 +0300

    Say "Hello, World!" With C++

---
+++ C++/Introduction/Say "Hello, World!" With C++/Solution.cpp
@@ -0,0 +1,28 @@
+/*
+
+This is a simple challenge to help you practice printing to stdout. You may also want to complete Solve Me First in C++ before attempting this challenge.
+
+We're starting out by printing the most famous computing phrase of all time! In the editor below, use either printf or cout to print the string Hello, World! to stdout.
+
+Input Format
+
+You do not need to read any input in this challenge.
+
+Output Format
+
+Print Hello, World! to stdout.
+
+Sample Output
+
+Hello, World!
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
