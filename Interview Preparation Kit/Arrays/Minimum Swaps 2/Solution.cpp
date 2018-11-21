b0d59d9caa57ba12d46a82cb5033107da6bf5e8c
Nikita Vasilev <nv3212@gmail.com>
2018-11-21 16:51:39 +0300

    Minimum Swaps 2

---
+++ Interview Preparation Kit/Arrays/Minimum Swaps 2/Solution.cpp
@@ -0,0 +1,126 @@
+/*
+
+You are given an unordered array consisting of consecutive integers ∈ [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+
+For example, given the array arr = [7, 1, 3, 2, 4, 5, 6] we perform the following steps:
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+
+It took 5 swaps to sort the array.
+
+Function Description
+
+Complete the function minimumSwaps in the editor below. It must return an integer representing the minimum number of swaps to sort the array.
+
+minimumSwaps has the following parameter(s):
+
+arr: an unordered array of integers
+
+Input Format
+
+The first line contains an integer, n, the size of arr. 
+The second line contains n space-separated integers arr[i].
+
+Constraints
+
+1 <= n <= 10^5
+1 <= arr[i] <= n
+
+Output Format
+
+Return the minimum number of swaps to sort the given array.
+
+Sample Input 0
+
+4
+4 3 1 2
+
+Sample Output 0
+
+3
+
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int minimumSwaps(vector<int> arr) {
+    int i, c = 0, n = arr.size();
+    for(i = 0; i < n; i++) {
+        if(arr[i] == (i + 1))
+            continue;
+        
+        swap(arr[i],arr[arr[i]-1]);
+        c++;
+        i--;
+    }
+    return c;
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
