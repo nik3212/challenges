5e2308a3dc9bd857986a05f8e37594c45210d440
Nikita <nv3212@gmail.com>
2019-04-19 14:04:19 +0300

    Weather Observation Station 10

---
+++ SQL/Basic Select/Weather Observation Station 10/Solution.sql
@@ -0,0 +1,7 @@
+/*
+
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+*/
+
+select distinct CITY from STATION where right(CITY, 1) not in ('a', 'e', 'i', 'o', 'u');
