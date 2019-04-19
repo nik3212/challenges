2e3a2c56dc126322c2ed51f5ce171281f043208e
Nikita <nv3212@gmail.com>
2019-04-19 14:15:31 +0300

    Employee Names

---
+++ SQL/Basic Select/Employee Names/Solution.sql
@@ -0,0 +1,8 @@
+/*
+
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+*/
+
+select NAME from Employee
+order by NAME ASC;
