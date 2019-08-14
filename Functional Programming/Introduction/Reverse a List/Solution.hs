d811db5149695e5db391ff92ae822e3d3d55e213
Nikita <nv3212@gmail.com>
2019-08-14 19:50:26 +0300

    Reverse a List

---
+++ Functional Programming/Introduction/Reverse a List/Solution.hs
@@ -0,0 +1,53 @@
+{-|
+
+You are given a list of N elements. Reverse the list without using the reverse function. The input and output portions will be handled automatically. You need to write a function with the recommended method signature.
+
+Input Format
+
+Each element, X, of the list is displayed on a separate line.
+
+Output Format
+
+The output is the reverse of the input list.
+
+Sample Input
+
+19
+22
+3
+28
+26
+17
+18
+4
+28
+0
+
+Sample Output
+
+0
+28
+4
+18
+17
+26
+28
+3
+22
+19
+
+Method Signature
+
+Number Of Parameters: 1
+Parameters: [list]
+Returns: List or Vector
+
+Constraints
+
+1 <= N <= 100
+0 <= X <= 100
+
+-}
+
+rev :: [Int] -> [Int]
+rev l = reverse l
