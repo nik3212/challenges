664636c597f33b8ac716c9f3ba99bd807d5b4c9a
Nikita <nv3212@gmail.com>
2019-11-11 22:04:23 +0300

    HackerRank. Top Earners

---
+++ HackerRank/SQL/Aggregation/Top Earners/Solution.sql
@@ -0,0 +1,7 @@
+/*
+
+We define an employee's total earnings to be their monthly salary x months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
+
+*/
+
+select (SALARY * MONTHS)as EARNINGS ,count(*) from EMPLOYEE group by 1 order by EARNINGS desc LIMIT 1;
