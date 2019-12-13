26aa1f1a64c2c3739114d70de7d84a1eec42118f
Nikita <nv3212@gmail.com>
2019-12-13 20:47:40 +0300

    HackerRank. Looping with Numbers

---
+++ HackerRank/Linux Shell/Bash/Looping with Numbers/Solution.sh
@@ -0,0 +1,26 @@
+# Use for loops to display the natural numbers from  to .
+#
+# Input Format
+#
+# There is no input
+#
+# Constraints
+#
+# Output Format
+#
+# 1
+# 2
+# 3
+# 4
+# 5
+# .
+# .
+# .
+# .
+# .
+# 50
+
+for i in {1..50}
+do
+    echo "$i"
+done
