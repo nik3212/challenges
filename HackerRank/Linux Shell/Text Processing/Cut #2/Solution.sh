a175113d307a4da1966a877abebef59dfd998719
Nikita <nv3212@gmail.com>
2019-12-27 19:03:28 +0300

    Cut #2

---
+++ HackerRank/Linux Shell/Text Processing/Cut #2/Solution.sh
@@ -0,0 +1,29 @@
+# Display the 2nd and 7th character from each line of text.
+#
+# Input Format
+#
+# A text file with N lines of ASCII text only.
+#
+# Constraints
+# 
+# 1 <= N <= 100
+#
+# Output Format
+#
+# The output should contain N lines. Each line should contain just two characters at the 2nd and the 7th position of the corresponding input line.
+#
+# Sample Input
+#
+# Hello
+# World
+# how are you
+#
+# Sample Output
+#
+# e
+# o
+# oe
+
+while read x; do
+    echo $x | cut -c2,7
+done
