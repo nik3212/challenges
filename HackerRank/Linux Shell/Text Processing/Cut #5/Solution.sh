399d50ab021358ec7ed82b1974426289d5639316
Nikita <nv3212@gmail.com>
2020-01-15 23:01:47 +0300

    HackerRank. Cut #5

---
+++ HackerRank/Linux Shell/Text Processing/Cut #5/Solution.sh
@@ -0,0 +1,18 @@
+# Given a tab delimited file with several columns (tsv format) print the first three fields.
+#
+# Input Format
+#
+# A tab-separated file with lines of ASCII text only.
+#
+# Constraints
+#
+# 1 <= N <= 100
+# 2 <= C <= 100
+#
+# (N is the number of lines of text in the input file and C is the number of columns of data in the file)
+#
+# Output Format
+#
+# The output should contain N lines. For each line in the input, print the first three fields.
+
+cut -f-3
