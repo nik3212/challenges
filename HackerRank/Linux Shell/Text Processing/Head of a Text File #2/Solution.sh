71fb0c89825135f30527ffbd681159e3cc640430
Nikita <nv3212@gmail.com>
2020-02-11 21:54:01 +0300

    HackerRank. Head of a Text File #2

---
+++ HackerRank/Linux Shell/Text Processing/Head of a Text File #2/Solution.sh
@@ -0,0 +1,13 @@
+# In this challenge, we practice using the head command to display the first n characters of a text file.
+#
+# Display the first 20 characters of an input file.
+#
+# Input Format
+#
+# A text file.
+#
+# Output Format
+#
+# Output the first 20 characters of the text file.
+
+head -c20
