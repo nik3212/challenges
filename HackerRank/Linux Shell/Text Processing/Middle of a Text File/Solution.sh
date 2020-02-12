3ba2a5e1d9f80a9b78c0ce4b4c49fb779348f2f7
Nikita <nv3212@gmail.com>
2020-02-12 22:52:59 +0300

    HackerRank. Middle of a Text File

---
+++ HackerRank/Linux Shell/Text Processing/Middle of a Text File/Solution.sh
@@ -0,0 +1,11 @@
+# Display the lines (from line number 12 to 22, both inclusive) of a given text file.
+#
+# Input Format
+#
+# A text file
+#
+# Output Format
+#
+# Display the lines (from line number 12 to 22, both inclusive) for the input file.
+
+head -n 22 | tail -n +12
