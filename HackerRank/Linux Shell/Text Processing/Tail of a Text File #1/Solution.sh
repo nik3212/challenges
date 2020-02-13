59ac3b46f229e5667212fdd82a221ea95511bbc7
Nikita <nv3212@gmail.com>
2020-02-13 22:44:53 +0300

    HackerRank. Tail of a Text File #1

---
+++ HackerRank/Linux Shell/Text Processing/Tail of a Text File #1/Solution.sh
@@ -0,0 +1,13 @@
+# In this challenge, we practice using the tail command to display the last n lines of a text file.
+#
+# Display the last 20 lines of an input file.
+#
+# Input Format
+#
+# A text file.
+#
+# Constraints
+#
+# Output the last 20 lines of the text file.
+
+tail -20
