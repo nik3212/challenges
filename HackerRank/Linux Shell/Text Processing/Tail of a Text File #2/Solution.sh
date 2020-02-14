72e4b808f015f86d8883af794b15bcc423d63dd0
Nikita Vasilev <nv3212@gmail.com>
2020-02-14 22:15:02 +0300

    HackerRank. Tail of a Text File #2

---
+++ HackerRank/Linux Shell/Text Processing/Tail of a Text File #2/Solution.sh
@@ -0,0 +1,12 @@
+# In this challenge, we practice using the tail command to display the last n characters of a text file.
+# Display the last 20 characters of an input file.
+#
+# Input Format
+#
+# A text file.
+#
+# Output Format
+#
+# Output the last 20 characters of the text file.
+
+tail -c20
