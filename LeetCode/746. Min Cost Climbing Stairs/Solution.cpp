c569da65364519df1ee9695a8f6d26dbf55a39dd
Nikita <nv3212@gmail.com>
2020-02-15 17:40:15 +0300

    LeetCode. 746. Min Cost Climbing Stairs

---
+++ LeetCode/746. Min Cost Climbing Stairs/Solution.cpp
@@ -0,0 +1,38 @@
+/*
+
+746. Min Cost Climbing Stairs
+
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+Example 1:
+
+Input: cost = [10, 15, 20]
+Output: 15
+
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+
+Example 2:
+
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+
+*/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int a = 0, b = 0;
+
+        for (size_t i = 0; i < cost.size(); ++i) {
+            int sum = cost[i] + min(a, b);
+            a = b;
+            b = sum;
+        }
+
+        return min(a, b);
+    }
+};
