fd89dabafff2aebe7ddd7ba931d40e110390b0e7
Nikita <nv3212@gmail.com>
2020-02-17 20:40:26 +0300

    HackerRank. 'Tr' Command #1

---
+++ HackerRank/Linux Shell/Text Processing/'Tr' Command #1/Solution.sh
@@ -0,0 +1,13 @@
+# In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+#
+# In a given fragment of text, replace all parentheses ( ) with box brackets [ ].
+#
+# Input Format
+#
+# A block of ASCII text.
+#
+# Output Format
+#
+# Output the text with all parentheses ( ) replaced with box brackets [ ].
+
+tr "()" "[]"
