5ff734009f98f179aa1e77f9d8b87736276f70ae
Nikita <nv3212@gmail.com>
2020-02-18 22:03:42 +0300

    HackerRank. 'Tr' Command #2

---
+++ HackerRank/Linux Shell/Text Processing/'Tr' Command #2/Solution.sh
@@ -0,0 +1,13 @@
+# In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+#
+# In a given fragment of text, delete all the lowercase characters a - z.
+#
+# Input Format
+#
+# A block of ASCII text.
+#
+# Output Format
+#
+# Delete all the lowercase characters in the given block of text.
+
+tr -d a-z
