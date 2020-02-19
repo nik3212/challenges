498dd48382f74c6fde229268cc43ab96f43a11ca
Nikita <nv3212@gmail.com>
2020-02-19 22:24:12 +0300

    HackerRank. 'Tr' Command #3

---
+++ HackerRank/Linux Shell/Text Processing/'Tr' Command #3/Solution.sh
@@ -0,0 +1,11 @@
+# In a given fragment of text, replace all sequences of multiple spaces with just one space.
+#
+# Input Format
+#
+# A block of ASCII text.
+#
+# Output Format
+#
+# Replace all sequences of multiple spaces with just one space.
+
+tr -s " "
