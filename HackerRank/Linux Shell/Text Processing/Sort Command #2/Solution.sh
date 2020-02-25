e1bb94f10b7c2f70f516831d1e8167c38fe2b560
Nikita <nv3212@gmail.com>
2020-02-25 22:01:27 +0300

    HackerRank. Sort Command #2

---
+++ HackerRank/Linux Shell/Text Processing/Sort Command #2/Solution.sh
@@ -0,0 +1,13 @@
+# In this challenge, we practice using the sort command to sort input in text or TSV formats.
+#
+# Given a text file, order the lines in reverse lexicographical order (i.e. Z-A instead of A-Z).
+#
+# Input Format
+#
+# A text file.
+#
+# Output Format
+#
+# Output the text file with the lines reordered in reverse lexicographical order.
+
+sort -r
