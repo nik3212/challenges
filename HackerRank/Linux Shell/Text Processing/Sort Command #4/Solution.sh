12db6da128b4b578c32fd9fdd95c8ec6d017810f
Nikita <nv3212@gmail.com>
2020-03-03 22:37:56 +0300

    HackerRank. Sort Command #4

---
+++ HackerRank/Linux Shell/Text Processing/Sort Command #4/Solution.sh
@@ -0,0 +1,11 @@
+# You are given a file of text, where each line contains a number (which may be either an integer or have decimal places). There will be no extra characters other than the number or the newline at the end of each line. Sort the lines in descending order - - such that the first line holds the (numerically) largest number and the last line holds the (numerically) smallest number.
+#
+# Input Format
+#
+# A text file where each line contains a number as described above.
+#
+# Output Format
+#
+# The text file, with lines re-ordered in descending order (numerically).
+
+sort -nr
