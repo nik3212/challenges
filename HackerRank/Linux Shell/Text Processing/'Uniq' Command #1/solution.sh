2fddd5a90bd70cea65f2e18cebc8b67fab995423
Nikita <nv3212@gmail.com>
2020-03-18 23:02:04 +0300

    HackerRank. 'Uniq' Command #1

---
+++ HackerRank/Linux Shell/Text Processing/'Uniq' Command #1/solution.sh
@@ -0,0 +1,7 @@
+# In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+#
+# Given a text file, remove the consecutive repetitions of any line.In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+#
+# Given a text file, remove the consecutive repetitions of any line.
+
+uniq
