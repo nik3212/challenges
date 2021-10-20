1e2206de116352c84c71bb0a4715627682f96dc4
Nikita Vasilev <nv3212@gmail.com>
2021-10-20 21:50:06 +0300

    LeetCode. 701. Insert into a Binary Search Tree

---
+++ LeetCode/701. Insert into a Binary Search Tree/solution_3.cpp
@@ -0,0 +1,51 @@
+/*
+
+701. Insert into a Binary Search Tree
+
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* temp = root;
+
+        if (root == nullptr) {
+            return new TreeNode(val);
+        }
+
+        while (true) {
+            if (temp->val < val) {
+                if (temp->right != nullptr) {
+                    temp = temp->right;
+                } else {
+                    temp->right = new TreeNode(val);
+                    return root;
+                }
+            } else {
+                if (temp->left != nullptr) {
+                    temp = temp->left;
+                } else {
+                    temp->left = new TreeNode(val);
+                    return root;
+                }
+            }
+        }
+
+        return root;
+    }
+};
