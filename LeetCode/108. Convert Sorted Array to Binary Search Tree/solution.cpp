df4183ad15a3a957d4f67504575d76b0d1857e04
Nikita Vasilev <nv3212@gmail.com>
2021-11-02 22:23:03 +0300

    LeetCode. 108. Convert Sorted Array to Binary Search Tree

---
+++ LeetCode/108. Convert Sorted Array to Binary Search Tree/solution.cpp
@@ -0,0 +1,34 @@
+/*
+
+108. Convert Sorted Array to Binary Search Tree
+
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* sortedArrayToBST(vector<int>& nums, int left, int right) {
+        if (right < left) {
+            return nullptr;
+        }
+        int m = (left + right) / 2;
+        return new TreeNode(nums[m], sortedArrayToBST(nums, left, m - 1), sortedArrayToBST(nums, m + 1, right));
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST(nums, 0, nums.size() - 1);
+    }
+};
