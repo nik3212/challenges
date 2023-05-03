a759dc2ee459e79c9e4d575466ec41ab1dac6b23
Nikita Vasilev <nv3212@gmail.com>
2023-05-03 08:10:58 +0400

    LeetCode. 101. Symmetric Tree

---
+++ LeetCode/101. Symmetric Tree/solution.swift
@@ -0,0 +1,53 @@
+/*
+
+101. Symmetric Tree
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func helper(_ left: TreeNode?, _ right: TreeNode?) -> Bool {
+        if (left == nil && right == nil) { return true }
+        if (left == nil || right == nil) { return false }
+        
+        return (left?.val == right?.val) 
+            && helper(left?.left, right?.right)
+            && helper(left?.right, right?.left)
+    }
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        helper(root?.left, root?.right)
+    }
+}
