5003be27712dda5d98f91001f1969830c417d8f3
Nikita Vasilev <nv3212@gmail.com>
2023-07-03 18:37:03 +0400

    LeetCode. 876. Middle of the Linked List

---
+++ LeetCode/876. Middle of the Linked List/solution.swift
@@ -0,0 +1,35 @@
+/*
+
+141. Linked List Cycle
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+
+        while (fast != nil && fast?.next != nil) {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        return slow
+    }
+}
