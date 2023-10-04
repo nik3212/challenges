c373c3a336638268e9fccd992e6343709d5c251e
Nikita Vasilev <nv3212@gmail.com>
2023-10-04 07:16:27 +0300

    LeetCode. 222. Count Complete Tree Nodes

---
+++ LeetCode/222. Count Complete Tree Nodes/solution.cpp
@@ -0,0 +1,36 @@
+/*
+
+222. Count Complete Tree Nodes
+
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int h(TreeNode* root) {
+        if (root == nullptr) { return 0; }
+        int l = countNodes(root->left);
+        int r = countNodes(root->right);
+        return 1 + l + r;
+    }
+public:
+    int countNodes(TreeNode* root) {
+        return h(root);
+    }
+};
