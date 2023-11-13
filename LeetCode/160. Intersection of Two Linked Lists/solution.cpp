d1d03c654e48072a1e180ee7a9f8ce6793848c42
Nikita Vasilev <nv3212@gmail.com>
2023-11-13 17:54:12 +0300

    LeetCode. 160. Intersection of Two Linked Lists

---
+++ LeetCode/160. Intersection of Two Linked Lists/solution.cpp
@@ -0,0 +1,52 @@
+/*
+
+160. Intersection of Two Linked Lists
+
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *h1 = headA;
+        ListNode *h2 = headB;
+
+        while (h1 != h2) {
+            if (h1 == nullptr) {
+                h1 = headB;
+            } else {
+                h1 = h1->next;
+            }
+
+            if (h2 == nullptr) {
+                h2 = headA;
+            } else {
+                h2 = h2->next;
+            }
+        }
+
+        return h1;
+    }
+};
