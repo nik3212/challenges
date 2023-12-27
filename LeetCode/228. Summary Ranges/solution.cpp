0b94c35e646e6d7f074b616c912aa496951c22a1
Nikita Vasilev <nv3212@gmail.com>
2023-12-27 20:19:18 +0100

    LeetCode. 228. Summary Ranges

--- LeetCode/228. Summary Ranges/solution.cpp
+++ LeetCode/228. Summary Ranges/solution.cpp
@@ -19,24 +19,26 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         std::vector<std::string> res;
+        std::string temp = "";
 
         int n = nums.size();
-        int prev = 0;
 
-        for (int i = 1; i <= nums.size(); ++i) {
-            if (nums[i] == nums[i - 1] + 1) {
-                continue;
-            } else {
-                int curr = i - 1;
+        for (int i = 0; i < nums.size(); ++i) {
+            int j = i;
 
-                if (curr == prev) {
-                    res.push_back(to_string(nums[prev]));
-                } else {
-                    res.push_back(to_string(nums[prev]) + "->" + to_string(nums[curr]));
-                }
-
-                prev = i;
+            while (j + 1 < nums.size() && nums[j + 1] == nums[j] + 1) {
+                j++;
             }
+
+            if (j > i) {
+                temp += to_string(nums[i]) + "->" + to_string(nums[j]); 
+            } else {
+                temp += to_string(nums[i]);
+            }
+
+            res.push_back(temp);
+            temp = "";
+            i = j;
         }
 
         return res;
