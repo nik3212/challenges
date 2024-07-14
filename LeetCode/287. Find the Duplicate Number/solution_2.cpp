8642e050ca47a5ed661e1189d6f725e4ec1338bb
Nikita Vasilev <nv3212@gmail.com>
2024-07-14 18:17:05 +0400

    LeetCode. 287. Find the Duplicate Number

---
+++ LeetCode/287. Find the Duplicate Number/solution_2.cpp
@@ -0,0 +1,33 @@
+/*
+
+287. Find the Duplicate Number
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do  {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
