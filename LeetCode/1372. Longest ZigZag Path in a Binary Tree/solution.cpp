579b78b98d5714ae8472e0d2437ed8e53a7ee367
Nikita Vasilev <nv3212@gmail.com>
2024-11-14 08:45:54 +0100

    LeetCode. 1372. Longest ZigZag Path in a Binary Tree

---
+++ LeetCode/1372. Longest ZigZag Path in a Binary Tree/solution.cpp
@@ -0,0 +1,47 @@
+/*
+
+1372. Longest ZigZag Path in a Binary Tree
+
+You are given the root of a binary tree.
+
+A ZigZag path for a binary tree is defined as follow:
+
+Choose any node in the binary tree and a direction (right or left).
+If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
+Change the direction from right to left or from left to right.
+Repeat the second and third steps until you can't move in the tree.
+Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+
+Return the longest ZigZag path contained in that tree.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int count = 0;
+
+    void dfs(TreeNode* root, int left, int right) {
+        if (root == nullptr) { return; }
+
+        count = std::max(count, std::max(left, right));
+
+        dfs(root->left, right + 1, 0);
+        dfs(root->right, 0, left + 1);
+    }
+public:
+    int longestZigZag(TreeNode* root) {
+        dfs(root, 0, 0);
+        return count;
+    }
+};
